4fdde6bc5506ead3f5d72c8c9e6f8e7ce612c0b7
gfardell <gemma.fardell@stfc.ac.uk>
2020-09-17 10:24:15 +0100

    Updated conda channels for jenkins

--- build/jenkins-build.sh
+++ build/jenkins-build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-export CCPI_BUILD_ARGS="-c conda-forge -c ccpi -c astra-toolbox"
+export CCPI_BUILD_ARGS="-c conda-forge -c ccpi -c astra-toolbox/label/dev"
 bash <(curl -L https://raw.githubusercontent.com/vais-ral/CCPi-VirtualMachine/master/scripts/jenkins-build.sh)
