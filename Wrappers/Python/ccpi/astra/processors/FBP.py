07360a27535866b135899f7540eae19c9d19d317
gfardell <gemma.fardell@stfc.ac.uk>
2020-11-04 17:38:31 +0000

    create FBP single proccessor

---
+++ Wrappers/Python/ccpi/astra/processors/FBP.py
@@ -0,0 +1,66 @@
+#========================================================================
+# Copyright 2019 Science Technology Facilities Council
+# Copyright 2019 University of Manchester
+#
+# This work is part of the Core Imaging Library developed by Science Technology
+# Facilities Council and University of Manchester
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0.txt
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#=========================================================================
+
+from ccpi.framework import DataProcessor
+from ccpi.astra.processors.FBP_Flexible import FBP_Flexible
+from ccpi.astra.processors.FDK_Flexible import FDK_Flexible
+from ccpi.astra.processors.FBP_Simple import FBP_Simple
+
+class FBP(DataProcessor):
+
+    def __init__(self, volume_geometry, sinogram_geometry, device='gpu'): 
+        
+        super(FBP, self).__init__( volume_geometry=volume_geometry, sinogram_geometry=sinogram_geometry, device=device, processor = False)  
+
+        if device is 'gpu':
+            if sinogram_geometry.geom_type == 'parallel':
+                processor = FBP_Flexible(volume_geometry, sinogram_geometry)
+            else:
+                processor = FDK_Flexible(volume_geometry, sinogram_geometry)
+            
+        else:
+            UserWarning("ASTRA back-projector running on CPU will not make use of enhanced geometry parameters")
+            if sinogram_geometry.dimension == '2D':
+                processor = FBP_Simple(volume_geometry, sinogram_geometry,  device='cpu') 
+            else:
+                raise NotImplementedError("Cannot process 3D data without a GPU")
+
+        if sinogram_geometry.channels > 1: 
+            raise NotImplementedError("Cannot process multi-channel data")
+            #processor_full = ChannelwiseProcessor(processor, self.sinogram_geometry.channels, dimension='prepend')
+            #self.processor = operator_full
+        
+        self.processor = processor
+
+    def set_input(self, dataset):       
+        return self.processor.set_input(dataset)
+
+    def get_input(self):       
+        return self.processor.get_input()
+
+    def get_output(self, out=None):       
+        return self.processor.get_output(out=None)
+
+    def check_input(self, dataset):       
+        return self.processor.check_input(dataset)
+        
+    def process(self, out=None):
+        return self.processor.process(out=None)
--- Wrappers/Python/ccpi/astra/processors/__init__.py
+++ Wrappers/Python/ccpi/astra/processors/__init__.py
@@ -6,6 +6,5 @@ from .AstraBackProjectorMC import AstraBackProjectorMC
 from .AstraForwardProjector3D import AstraForwardProjector3D
 from .AstraBackProjector3D import AstraBackProjector3D
 from .FBP import FBP
-from .FDK import FDK
 from .AstraForwardProjectorVec import AstraForwardProjectorVec
 from .AstraBackProjectorVec import AstraBackProjectorVec
