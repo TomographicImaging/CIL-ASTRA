01ee37d3bf5d376170fdc655c7bbebf100dd332f
Edoardo Pasca <edo.paskino@gmail.com>
2021-02-12 18:14:51 +0000

    link arrays to GPU astra

--- Wrappers/Python/cil/plugins/astra/processors/AstraBackProjectorVec.py
+++ Wrappers/Python/cil/plugins/astra/processors/AstraBackProjectorVec.py
@@ -1,7 +1,8 @@
 from cil.framework import DataProcessor, ImageData
 from cil.plugins.astra.utilities import convert_geometry_to_astra_vec
 import astra
-import numpy
+from astra import astra_dict, algorithm, data3d
+import numpy as np
 
 
 class AstraBackProjectorVec(DataProcessor):
@@ -98,22 +99,75 @@ class AstraBackProjectorVec(DataProcessor):
         if len(DATA.shape) == 2:
             #for 2D cases
             pad = True
-            data_temp = numpy.expand_dims(DATA.as_array(),axis=0)
+            data_temp = np.expand_dims(DATA.as_array(),axis=0)
         else:
             data_temp = DATA.as_array()
 
-        rec_id, arr_out = astra.create_backprojection3d_gpu(data_temp,
+        if out is None:
+            rec_id, arr_out = self.create_backprojection3d_gpu(data_temp,
                             self.proj_geom,
                             self.vol_geom)
+        else:
+            if pad:
+                arr_out = np.expand_dims(out.as_array(), axis=0)
+            else:
+                arr_out = out.as_array()
+            rec_id = astra.data3d.link('-vol', self.vol_geom, arr_out)
+            self.create_backprojection3d_gpu( data_temp, self.proj_geom, self.vol_geom, False, rec_id)
 
+        # delete the GPU copy
         astra.data3d.delete(rec_id)
         
         if pad is True:
-            arr_out = numpy.squeeze(arr_out, axis=0)
+            arr_out = np.squeeze(arr_out, axis=0)
 
         if out is None:
             out = ImageData(arr_out, deep_copy=False, geometry=self.volume_geometry.copy(), suppress_warning=True)
             return out
         else:
             out.fill(arr_out)
+
+    def create_backprojection3d_gpu(data, proj_geom, vol_geom, returnData=True, vol_id=None):
+        """Create a backprojection of a sinogram (3D) using CUDA.
+            :param data: Sinogram data or ID.
+            :type data: :class:`numpy.ndarray` or :class:`int`
+            :param proj_geom: Projection geometry.
+            :type proj_geom: :class:`dict`
+            :param vol_geom: Volume geometry.
+            :type vol_geom: :class:`dict`
+            :param returnData: If False, only return the ID of the backprojection.
+            :type returnData: :class:`bool`
+            :param vol_id: ID of the np array linked with astra
+            :type vol_id: int, default None
+            :returns: :class:`int` or (:class:`int`, :class:`numpy.ndarray`) -- If ``returnData=False``, returns the ID of the backprojection. Otherwise, returns a tuple containing the ID of the backprojection and the backprojection itself, in that order.
+        """
+        if isinstance(data, np.ndarray):
+            sino_id = data3d.create('-sino', proj_geom, data)
+        else:
+            sino_id = data
+
+        if vol_id is None:
+            vol_id = data3d.create('-vol', vol_geom, 0)
+
+        cfg = astra_dict('BP3D_CUDA')
+        cfg['ProjectionDataId'] = sino_id
+        cfg['ReconstructionDataId'] = vol_id
+        alg_id = algorithm.create(cfg)
+        algorithm.run(alg_id)
+        algorithm.delete(alg_id)
+
+        if isinstance(data, np.ndarray):
+            data3d.delete(sino_id)
+
+        if vol_id is not None:
+            if returnData:
+                return vol_id, data3d.get_shared(vol_id)
+            else:
+                return vol_id
+        else:
+            if returnData:
+                return vol_id, data3d.get(vol_id)
+            else:
+                return vol_id
+
             
--- Wrappers/Python/cil/plugins/astra/processors/AstraForwardProjectorVec.py
+++ Wrappers/Python/cil/plugins/astra/processors/AstraForwardProjectorVec.py
@@ -1,7 +1,7 @@
 from cil.framework import DataProcessor, AcquisitionData
 from cil.plugins.astra.utilities import convert_geometry_to_astra_vec
-import astra
-import numpy
+from astra import astra_dict, algorithm, data3d
+import numpy as np
 
 class AstraForwardProjectorVec(DataProcessor):
     '''AstraForwardProjectorVec
@@ -94,21 +94,78 @@ class AstraForwardProjectorVec(DataProcessor):
         if len(IM.shape) == 2:
             #for 2D cases
             pad = True
-            data_temp = numpy.expand_dims(IM.as_array(),axis=0)
+            data_temp = np.expand_dims(IM.as_array(),axis=0)
         else:
             data_temp = IM.as_array()
 
-        sinogram_id, arr_out = astra.create_sino3d_gpu(data_temp, 
+        if out is None:
+
+            sinogram_id, arr_out = astra.create_sino3d_gpu(data_temp, 
                                                            self.proj_geom,
                                                            self.vol_geom)
+        else:
+            if pad:
+                arr_out = np.expand_dims(out.as_array(), axis=0)
+            else:
+                arr_out = out.as_array()
+                
+            sinogram_id = astra.data3d.link('-sino', self.proj_geom, arr_out)
+            self.create_backprojection3d_gpu(data_temp, self.proj_geom, self.vol_geom, False, sinogram_id)
 
+        #clear the memory on GPU
         astra.data3d.delete(sinogram_id)
         
         if pad is True:
-            arr_out = numpy.squeeze(arr_out, axis=0)
+            arr_out = np.squeeze(arr_out, axis=0)
 
         if out is None:
             out = AcquisitionData(arr_out, deep_copy=False, geometry=self.sinogram_geometry.copy(), suppress_warning=True)
             return out
         else:
             out.fill(arr_out)
+
+    def create_sino3d_gpu(data, proj_geom, vol_geom, returnData=True, gpuIndex=None, sino_id=None):
+        """Create a forward projection of an image (3D).
+    :param data: Image data or ID.
+    :type data: :class:`numpy.ndarray` or :class:`int`
+    :param proj_geom: Projection geometry.
+    :type proj_geom: :class:`dict`
+    :param vol_geom: Volume geometry.
+    :type vol_geom: :class:`dict`
+    :param returnData: If False, only return the ID of the forward projection.
+    :type returnData: :class:`bool`
+    :param gpuIndex: Optional GPU index.
+    :type gpuIndex: :class:`int`
+    :returns: :class:`int` or (:class:`int`, :class:`numpy.ndarray`) -- If ``returnData=False``, returns the ID of the forward projection. Otherwise, returns a tuple containing the ID of the forward projection and the forward projection itself, in that order.
+    """
+
+        if isinstance(data, np.ndarray):
+            volume_id = data3d.create('-vol', vol_geom, data)
+        else:
+            volume_id = data
+        if sino_id is None:
+            sino_id = data3d.create('-sino', proj_geom, 0)
+
+        algString = 'FP3D_CUDA'
+        cfg = astra_dict(algString)
+        if not gpuIndex==None:
+            cfg['option']={'GPUindex':gpuIndex}
+        cfg['ProjectionDataId'] = sino_id
+        cfg['VolumeDataId'] = volume_id
+        alg_id = algorithm.create(cfg)
+        algorithm.run(alg_id)
+        algorithm.delete(alg_id)
+
+        if isinstance(data, np.ndarray):
+            data3d.delete(volume_id)
+        if sino_id is None:
+            if returnData:
+                return sino_id, data3d.get(sino_id)
+            else:
+                return sino_id
+        else:
+            if returnData:
+                return sino_id, data3d.get_shared(sino_id)
+            else:
+                return sino_id
+
